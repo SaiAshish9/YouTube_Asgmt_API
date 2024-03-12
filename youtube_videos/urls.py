7453c83bac1c693693abc58563ccd0e7ec162bd7
saiashish9 <saiashish3760@gmail.com>
2024-03-12 21:04:10 +0530

    [32aa100dbd8a4479878f174ad8f9d990]-added search functionality

--- youtube_videos/urls.py
+++ youtube_videos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name='youtube_videos'
 urlpatterns=[
-   path('', views.VideotList.as_view())
+   path('', views.VideotList.as_view()),
+   path('search/', views.VideoSearchView.as_view())
 ]
--- youtube_videos/views.py
+++ youtube_videos/views.py
@@ -1,10 +1,17 @@
 from .models  import Video
 from .serializers import VideoSerializer
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import GenericAPIView, ListAPIView
 from rest_framework.mixins import ListModelMixin
+from rest_framework.filters import SearchFilter
 
 class VideotList(GenericAPIView, ListModelMixin):
     queryset = Video.objects.all()
     serializer_class = VideoSerializer
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
+    
+class VideoSearchView(ListAPIView):
+    queryset = Video.objects.all()
+    serializer_class = VideoSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['title', 'description']
