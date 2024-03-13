a34b90e3fc0861bfcaaf15f0ecff4645eb882aba
saiashish9 <saiashish3760@gmail.com>
2024-03-13 06:04:16 +0530

    [32aa100dbd8a4479878f174ad8f9d990]-updated view

--- youtube_videos/views.py
+++ youtube_videos/views.py
@@ -5,7 +5,7 @@ from rest_framework.mixins import ListModelMixin
 from rest_framework.filters import SearchFilter
 
 class VideotList(GenericAPIView, ListModelMixin):
-    queryset = Video.objects.all()
+    queryset = Video.objects.all().order_by('-publishing_date')
     serializer_class = VideoSerializer
     def get(self, request, *args, **kwargs):
         return self.list(request, *args, **kwargs)
